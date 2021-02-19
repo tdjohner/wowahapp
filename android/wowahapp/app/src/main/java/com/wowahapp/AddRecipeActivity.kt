8fe0b55f4bceb38540be722a86bc2d3e15bb2c30
Teagan Johner <tdjohner@gmail.com>
2021-02-19 01:49:42 -0700

    Bassic http request to our API proxy.

--- android/wowahapp/app/src/main/java/com/wowahapp/AddRecipeActivity.kt
+++ android/wowahapp/app/src/main/java/com/wowahapp/AddRecipeActivity.kt
@@ -6,8 +6,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.Request.Method
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+
 import kotlinx.android.synthetic.*
+import org.json.JSONObject
+import java.net.URLEncoder
 
 class AddRecipeActivity : AppCompatActivity() {
 
@@ -23,21 +32,31 @@ class AddRecipeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_recipe)
 
         serverSelect = findViewById<Spinner>(R.id.serverSelectSpinner) as Spinner
+        professionSelect = findViewById<Spinner>(R.id.professionSelectSpinner) as Spinner
+        recipeListView = findViewById<RecyclerView>(R.id.recyclerView) as RecyclerView
+        confirmButton = findViewById<Button>(R.id.confirmButton) as Button
 
         val exampleServers = arrayOf("Please_select_item", "Broken Fishing Pole", "Keen Incisor", "Blackthorn Warboots", "Potion of Deathly Fixation")
         serverSelect.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,exampleServers)
-
-        professionSelect = findViewById<Spinner>(R.id.professionSelectSpinner) as Spinner
-
-        recipeListView = findViewById<RecyclerView>(R.id.recyclerView) as RecyclerView
-
-        confirmButton = findViewById<Button>(R.id.confirmButton) as Button
         confirmButton.setOnClickListener {
 
             dummyText = findViewById<TextView>(R.id.dummyTextView) as TextView
-            dummyText.text = serverSelect.getItemAtPosition(serverSelect.selectedItemPosition).toString()
 
-            //Volley.newRequestQueue(this)
+            val q = Volley.newRequestQueue(this)
+
+            //var url = "http://wowahapp.com/getitem/" + serverSelect.getItemAtPosition(serverSelect.selectedItemPosition).toString().replace(" ", "%20")
+            val url = "http://wowahapp.com/getitem/Broken%20Fishing%20Pole/"
+            println(url)
+            val stringRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener<JSONObject> { response ->
+                val j = response.getString("Name")
+                dummyText.text = j
+                },
+                Response.ErrorListener { error ->
+                    println(error)
+                })
+            q.add(stringRequest)
+
+
 
             /* RE-IMPLEMENT AFTER DEMO!
             val homeActivityIntent = Intent(this, HomeActivity::class.java)
