2dc81a37e24a8d5c1e29daec54bd30a2318fb4e4
Bhavish Bhunjun <48217282+bhunjun@users.noreply.github.com>
2021-03-19 14:47:29 -0600

    Added authentication using Auth0

--- android/wowahapp/app/src/main/java/com/wowahapp/HomeActivity.kt
+++ android/wowahapp/app/src/main/java/com/wowahapp/HomeActivity.kt
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import com.auth0.android.result.UserProfile
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,10 +21,19 @@ class HomeActivity : AppCompatActivity() {
     lateinit var logoutTextView : TextView
     private val itemsList = ArrayList<RecipeModel>()
     private lateinit var customAdapter: CustomAdapter
+    private lateinit var account : Auth0
+    private var cachedCredentials: Credentials? = null
+    private var cachedUserProfile: UserProfile? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Initialize the account settings
+        account = Auth0(
+            getString(R.string.com_auth0_clientId),
+            getString(R.string.com_auth0_domain)
+        )
 
         addRecipeButton = findViewById<Button>(R.id.addRecipeButton) as Button
         addRecipeButton.setOnClickListener{
@@ -27,8 +43,12 @@ class HomeActivity : AppCompatActivity() {
 
         logoutTextView = findViewById<TextView>(R.id.logoutTextView)
         logoutTextView.setOnClickListener {
-            val mainActivityIntent = Intent(this, MainActivity::class.java)
-            startActivity(mainActivityIntent)
+            // log user out
+            logout()
+
+            // go back to main page
+//            val mainActivityIntent = Intent(this, MainActivity::class.java)
+//            startActivity(mainActivityIntent)
         }
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView3)
@@ -47,5 +67,26 @@ class HomeActivity : AppCompatActivity() {
         customAdapter.addItem(recipe)
         recipe = RecipeModel("1","1","1","1")
         customAdapter.addItem(recipe)
+    }
+
+    private fun logout() {
+        WebAuthProvider.logout(account)
+            .withScheme(getString(R.string.com_auth0_scheme))
+            .start(this, object: Callback<Void?, AuthenticationException> {
+                override fun onSuccess(result: Void?) {
+                    cachedCredentials = null
+                    cachedUserProfile = null
+
+                    Toast.makeText(this@HomeActivity, "Logged out", Toast.LENGTH_SHORT).show()
+
+                    // Go back to the main page
+                    intent = Intent(this@HomeActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                override fun onFailure(error: AuthenticationException) {
+                    Toast.makeText(this@HomeActivity, "\"Failure: ${error.getCode()}\"", Toast.LENGTH_SHORT).show()
+                }
+            })
     }
 }
--- android/wowahapp/app/src/main/java/com/wowahapp/MainActivity.kt
+++ android/wowahapp/app/src/main/java/com/wowahapp/MainActivity.kt
@@ -8,6 +8,16 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.animation.LinearInterpolator
 import android.widget.TextView
+import android.widget.Toast
+import android.os.Vibrator
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import com.auth0.android.result.UserProfile
+import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,10 +25,20 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var registerUser : TextView
     lateinit var forgotPass : TextView
+    private lateinit var account : Auth0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize vibration
+        val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
+
+        // Initialize the account settings
+        account = Auth0(
+                getString(R.string.com_auth0_clientId),
+                getString(R.string.com_auth0_domain)
+        )
 
         // https://stackoverflow.com/questions/47298935/handling-enter-key-on-edittext-kotlin-android
         editPassword.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
@@ -52,8 +72,51 @@ class MainActivity : AppCompatActivity() {
         //sendJson()
     }
 
-    // Validate user against user database
+    // Validate user using Auth0 service
     private fun sendLoginRequest() {
+        WebAuthProvider.login(account)
+                .withScheme(getString(R.string.com_auth0_scheme))
+                .withScope("openid profile email")
+                // Launch the authentication passing the callback where the results will be received
+                .start(this,  object : Callback<Credentials, AuthenticationException> {
+                    // Called when there is an authentication failure
+                    override fun onFailure(error: AuthenticationException) {
+                        Toast.makeText(this@MainActivity, "\"Failure: ${error.getCode()}\"", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // Called when authentication completed successfully
+                    override fun onSuccess(result: Credentials) {
+                        // Get the access token from the credentials object.
+                        // This can be used to call APIs
+                        val accessToken = result.accessToken
+
+                        val homeIntent = Intent(this@MainActivity, HomeActivity::class.java)
+                        startActivity(homeIntent)
+
+                        Toast.makeText(this@MainActivity, "Logged in", Toast.LENGTH_SHORT).show()
+                        showUserProfile(accessToken)
+
+                    }
+                })
+    }
+
+    private fun showUserProfile(accessToken: String) {
+        var client = AuthenticationAPIClient(account)
+
+        // With the access token, call `userInfo` and get the profile from Auth0.
+        client.userInfo(accessToken)
+                .start(object : Callback<UserProfile, AuthenticationException> {
+                    override fun onFailure(error: AuthenticationException) {
+                        Toast.makeText(this@MainActivity, "\"Failure: ${error.getCode()}\"", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onSuccess(result: UserProfile) {
+                        // We have the user's profile!
+                        val email = result.email
+                        val name = result.nickname
+                        Toast.makeText(this@MainActivity, email + "\n" + name, Toast.LENGTH_SHORT).show()
+                    }
+                })
     }
 
     // login wallpaper moving background from https://stackoverflow.com/questions/36894384/android-move-background-continuously-with-animation
